320b92f4ca6d1b077b04dfec08ca1dee840c4606
florin <dx9eng@yahoo.com>
2012-06-19 12:33:05 +0300

    Add css files and admin login view.

---
+++ views/admin_login_view.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<link rel="stylesheet" type="text/css" href="../css/288433.css" />
+</head>
+<body>
+    <div id="wrapper">
+        <div id="headerwrap">
+        <div id="header">
+        </div>
+        </div>
+        <div id="navigationwrap">
+        <div id="navigation">
+        </div>
+        </div>
+        <div id="contentwrap">
+        <div id="content">
+        </div>
+        </div>
+        <div id="rightcolumnwrap">
+        <div id="rightcolumn">
+        </div>
+        </div>
+        <div id="footerwrap">
+        <div id="footer">
+        </div>
+        </div>
+    </div>
+</body>
+</html>
