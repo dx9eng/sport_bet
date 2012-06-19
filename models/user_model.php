6e84264a5da1583942726d2e8035450453b2fa8e
Flavia Fodor <fflavy2004@yahoo.com>
2012-06-19 16:50:33 +0300

    from post to posts

--- models/user_model.php
+++ models/user_model.php
@@ -9,21 +9,21 @@ class UserModel {
 		$this->db = new PDO(DB_INFO, DB_USER, DB_PASS);
 	}
 
-	public function loginUser($_POST) {
+	public function loginUser($_POSTs) {
 
 		
         
 		$sql = "SELECT account_type,COUNT(*) AS num_users FROM user WHERE email=? AND password=?";
 		$stmt = $this->db->prepare($sql);
-		$stmt->execute(array($_POST['useremail'], $_POST['password']));
+		$stmt->execute(array($_POSTs['useremail'], $_POSTs['password']));
 		$response = $stmt->fetch();
 		if ($response['num_users'] > 0 && $response['account_type']==0) {
-			$_SESSION['admin'] = $_POST['useremail'];
+			$_SESSION['admin'] = $_POSTs['useremail'];
 			header('Location: /sport_bet/admin/');
 		   exit;
 		}
 		else if($response['num_users'] > 0 && $response['account_type']==1){
-			$_SESSION['user'] = $_POST['useremail'];
+			$_SESSION['user'] = $_POSTs['useremail'];
 			//die($_SESSION['user']);
 			header('Location: /sport_bet/user/');
 		   exit;
