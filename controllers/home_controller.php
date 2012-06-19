2bf3276c39fe79e6fb35ddddb831c03945c4306b
Flavia Fodor <fflavy2004@yahoo.com>
2012-06-19 15:43:08 +0300

    modify home controller

--- controllers/home_controller.php
+++ controllers/home_controller.php
@@ -5,19 +5,9 @@ class home {
 	public function index($params) {
 
 		// Instantiate the admin
-		include_once 'models/home_model.php';
-		$home_model = new HomeModel;
-		if ($_SERVER['REQUEST_METHOD'] == 'POST'
-			&& $_POST['action'] == 'login'
-			&& !empty($_POST['usermail'])
-			&& !empty($_POST['password'])) {
-			$home = $home_model->login();
-	}
-	else {
-		include 'views/index_login_view.php';
-	}
+	
 		// Include view
-		// include 'views/user_login_view.php';
+		include 'views/user_login_view.php';
 	}
 
 	public function login() {
