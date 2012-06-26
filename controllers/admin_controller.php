0ab4555745640b63a5e44b0c279f64f430fcab73
florin <dx9eng@yahoo.com>
2012-06-26 15:26:32 +0300

    fixup! Check if email exists.

--- controllers/admin_controller.php
+++ controllers/admin_controller.php
@@ -7,20 +7,6 @@ class admin {
 		// Instantiate match model
 		include_once 'models/match_model.php'; 
 		$match_model = new match;
-		//die($params);
-		// Retrieve list of entries
-		$matches = $match_model->getAllMatches();
-		
-		// Include view
-<?php
-
-class admin {
-
-	public function index($params) {
-
-		// Instantiate match model
-		include_once 'models/match_model.php'; 
-		$match_model = new match;
 		
 		// Retrieve list of entries
 		$matches = $match_model->getAllMatches();
